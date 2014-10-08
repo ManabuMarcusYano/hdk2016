c833100d412a4d1873c45b47a99cbfc1ee1ddf32
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-08 16:36:45 +0900

    first commit

---
+++ mock_store/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=270, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="stylesheet" type="text/css" href="https://dp2k75jeqnjha.cloudfront.net/kdj4s/assets/css/game/reset.css" />
+	<link rel="stylesheet" type="text/css" href="https://dp2k75jeqnjha.cloudfront.net/kdj4s/assets/css/game/common.css" />		
+	<link rel="stylesheet" type="text/css" href="https://dp2k75jeqnjha.cloudfront.net/kdj4s/assets/css/game/help.css" />	
+</head>
+<body>
+<a>Mock App</a>
+</body>
+</html>
