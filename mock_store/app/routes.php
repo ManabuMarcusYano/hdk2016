b11867bf4419cbceb1feea6e6b56d9b9c557e7b3
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-12 22:41:06 +0900

    Laravel導入

---
+++ mock_store/app/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make("header");
+});
+
+Route::get('about', function()
+{
+	return "このサイトについて";
+});
+
+Route::get('page', 'PageController@index');
+
+Route::get('page/{page}', 'PageController@showPage');
