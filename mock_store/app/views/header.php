4a447be2e84649523e355d141e3255ae06ec06ab
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-14 01:51:27 +0900

    ヘッダレイアウト

--- mock_store/app/views/header.php
+++ mock_store/app/views/header.php
@@ -1,4 +1,9 @@
-<p>ヘッダだよ</p>
-<a>左</a>
-<a>ロゴ</a>
-<a>右</a>
+<link rel="stylesheet" type="text/css" href="../../public/css/reset.css" />
+<link rel="stylesheet" type="text/css" href="../../public/css/common.css" />
+<header class = "header">
+	<ul class = "header_box">
+        <li class="list_category"><img src="img/icon_category.png" class = "icon_category" /></li>
+        <li class="list_logo"><img src="img/logo.png" class = "header_logo"/></li>
+        <li class="list_search"><img src="img/icon_search.png" class = "list_search" /></li>
+    </ul>
+</header>
--- mock_store/app/views/login.php
+++ mock_store/app/views/login.php
@@ -1,8 +1,4 @@
 <?php echo $head; ?>
-    <link rel="stylesheet" type="text/css" href="../../public/css/reset.css" />
-    <link rel="stylesheet" type="text/css" href="../../public/css/common.css" />
-    <link rel="stylesheet" type="text/css" href="../../public/css/login.css" />
-
 <img src="img/logo.png" class="login_logo" />
 <div class = "login_box">
 	<form action = "/">
@@ -10,7 +6,6 @@
     <input type="text" name="password" maxlength="20" placeholder="Password" class = "password_box" />
     <input type="submit" value="LOGIN" class = "login_button" />
     </form>
-	<!--<div class = "login_button"><p>LOGIN</p></div>-->
 </div>
 <p class = "can_not_login"><a href="">ログインができない方</a></p>
 <?php //echo $footer; ?>
