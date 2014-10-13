f77b7c773af8c0061674bdd85e8c03e9eb442c1f
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-13 19:02:13 +0900

    ログイン画面修正

--- mock_store/app/views/login.php
+++ mock_store/app/views/login.php
@@ -4,10 +4,13 @@
     <link rel="stylesheet" type="text/css" href="../../public/css/login.css" />
 
 <img src="img/logo.png" class="login_logo" />
-<div class = "login_box">    
-	<div class = "id_box"><p>ID</p></div>
-	<div class = "password_box"><p>Password</p></div>
-	<div class = "login_button"><p>LOGIN</p></div>
+<div class = "login_box">
+	<form action = "/">
+	<input type="text" name="id" maxlength="20" placeholder="ID" class = "id_box" />
+    <input type="text" name="password" maxlength="20" placeholder="Password" class = "password_box" />
+    <input type="submit" value="LOGIN" class = "login_button" />
+    </form>
+	<!--<div class = "login_button"><p>LOGIN</p></div>-->
 </div>
 <p class = "can_not_login"><a href="">ログインができない方</a></p>
 <?php //echo $footer; ?>
