bd4fcee78e8e41580d21df313f63aa2240a4029d
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-20 03:05:21 +0900

    ソート、検索、ユーザーデータ表示

--- mock_store/app/views/header.php
+++ mock_store/app/views/header.php
@@ -1,9 +1,26 @@
 <header class = "header">
 	<ul class = "header_box">
-        <li class="list_category"><a href = ""><img src="img/icon_category.png" class = "icon_category" /></a></li>
+        <li class="list_category"><a><img src="img/icon_category.png" class = "icon_category" /></a></li>
         <li class="list_logo"><a href = ""><img src="img/logo.png" class = "header_logo"/></a></li>
-        <li class="list_search"><a href = ""><img src="img/icon_search.png" class = "icon_search" /></a></li>
-        <li class="list_user"><a href = ""><img src="img/icon_search.png" class = "icon_user" /></a></li>
+        <li class="list_search"><a><img src="img/icon_search.png" class = "icon_search" /></a></li>
+        <li class="list_user"><a><img src="img/icon_search.png" class = "icon_user" /></a></li>
     </ul>
 </header>
 <div class="header_break"></div>
+<div id = "modal_screen">
+    <div class = "sorts modal_sort">
+        <div class = "sort"><a>開発開始日が新しい順</a></div>
+        <hr class = "separation" />
+        <div class = "sort"><a>開発開始日が古い順</a></div>
+        <hr class = "separation" />
+        <div class = "sort"><a>アップデート日が新しい順</a></div>
+        <hr class = "separation" />
+        <div class = "sort"><a>アップデート日が古い順</a></div>
+    </div>
+    <div class = "sorts modal_search">
+        <div class = "sort"><a>サーチ</a></div>
+    </div>
+    <div class = "sorts modal_user">
+        <div class = "sort"><a>ユーザー</a></div>
+    </div>
+</div>
--- mock_store/public/js/ui.js
+++ mock_store/public/js/ui.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
 	
 	// レート
 	$(".app_star").raty({
-		halfShow : true,
-		readOnly: true,
+		halfShow     : true,
+		readOnly     : true,
 		starHalf     : 'lib/raty-2.7.0/lib/images/star-half.png',
    		starOff      : 'lib/raty-2.7.0/lib/images/star-off.png',
     	starOn       : 'lib/raty-2.7.0/lib/images/star-on.png',
@@ -31,4 +31,51 @@ $(document).ready(function() {
     	return $(this).attr('data-score');
   		}
 	});
+
+	// ヘッダ
+	var isShowingModal = false;
+
+	$(".list_category").click(function(){
+		if(isShowingModal == false){
+			isShowingModal = true;
+			$(".modal_sort").show();
+			$("#modal_screen").show();
+			$(".list_category").children("a").children("img").attr({ src : "img/icon_search.png"});
+		}else{
+			isShowingModal = false;
+			$(".modal_sort").hide();
+			$(".modal_search").hide();
+			$(".modal_user").hide();
+			$("#modal_screen").hide();
+			$(".list_category").children("a").children("img").attr({ src : "img/icon_category.png"});
+			$(".list_search").css("visibility" , "visible");
+			$(".list_user").css("visibility" , "visible");
+		}
+	});
+
+	$(".list_search").click(function(){
+		if(isShowingModal == false){
+			isShowingModal = true;
+			$(".modal_search").show();
+			$("#modal_screen").show();
+			$(".list_category").children("a").children("img").attr({ src : "img/icon_search.png"});
+			$(".list_search").css("visibility" , "hidden");
+			$(".list_user").css("visibility" , "hidden");
+		}
+	});
+
+	$(".list_user").click(function(){
+		if(isShowingModal == false){
+			isShowingModal = true;
+			$(".modal_user").show();
+			$("#modal_screen").show();
+			$(".list_category").children("a").children("img").attr({ src : "img/icon_search.png"});
+			$(".list_search").css("visibility" , "hidden");
+			$(".list_user").css("visibility" , "hidden");
+		}
+	});
+
+	function hideOtherButtons(){
+
+	}
 });
