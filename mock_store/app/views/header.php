955fd40802fe847e0280bccf8e78c33e7ee3f572
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-19 19:48:22 +0900

    ヘッダレイアウト修正

--- mock_store/app/views/header.php
+++ mock_store/app/views/header.php
@@ -1,8 +1,13 @@
+<link rel="stylesheet" type="text/css" href="../../public/css/reset.css" />
+<link rel="stylesheet" type="text/css" href="../../public/css/common.css" />
+<link rel="stylesheet" type="text/css" href="../../public/css/login.css" />
+    
 <header class = "header">
 	<ul class = "header_box">
         <li class="list_category"><a href = ""><img src="img/icon_category.png" class = "icon_category" /></a></li>
         <li class="list_logo"><a href = ""><img src="img/logo.png" class = "header_logo"/></a></li>
-        <li class="list_search"><a href = ""><img src="img/icon_search.png" class = "list_search" /></a></li>
+        <li class="list_search"><a href = ""><img src="img/icon_search.png" class = "icon_search" /></a></li>
+        <li class="list_user"><a href = ""><img src="img/icon_search.png" class = "icon_user" /></a></li>
     </ul>
 </header>
 <div class="header_break"></div>
