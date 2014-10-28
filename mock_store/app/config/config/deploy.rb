fc1c88f0b871a15e9e041006382053a452aafce6
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-29 04:12:56 +0900

    デプロイ準備

--- mock_store/app/config/config/deploy.rb
+++ mock_store/app/config/config/deploy.rb
@@ -5,19 +5,19 @@ set :application, 'mock_store'
 set :repo_url, 'git@gitlab.abot.sh:mock-store/server.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/cj0054/www'
 
 # Default value for :scm is :git
 set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
@@ -32,7 +32,7 @@ set :scm, :git
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
--- mock_store/app/config/config/deploy/development.rb
+++ mock_store/app/config/config/deploy/development.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{mockstore.apb-shd-402.a4c.jp}
+role :web, %w{mockstore.apb-shd-402.a4c.jp}
+role :db,  %w{mockstore.apb-shd-402.a4c.jp}
 
 
 # Extended Server Syntax
