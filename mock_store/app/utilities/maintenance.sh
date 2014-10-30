a5142ead2f3802cec7fe2703501ee3e2e83d1138
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-10-31 01:06:05 +0900

    メンテシェル修正

--- mock_store/app/utilities/maintenance.sh
+++ mock_store/app/utilities/maintenance.sh
@@ -3,6 +3,16 @@
 # $1 development, staging, production
 # $2 up, down
 
+if [ $1 != "production" -a $1 != "staging" -a $1 != "development" ]; then
+	echo "Illegal envirnment setting!!"
+	exit 0
+fi
+
+if [ $2 != "up" -a $2 != "down" ]; then
+	echo "Illegal envirnment setting!!"
+	exit 0
+fi
+
 hostname="mockstore.apb-shd-402.a4c.jp"
 username="root"
 dir="~/../home/cj0054/www/$1"
