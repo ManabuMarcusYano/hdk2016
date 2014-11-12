2cde079c5969140e51163b975eec53c46282d240
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-11-12 14:15:54 +0900

    セッションにログイン情報を記憶

--- mock_store/app/controllers/LogInOutContoller.php
+++ mock_store/app/controllers/LogInOutContoller.php
@@ -4,6 +4,8 @@ class LogInOutController extends BaseController{
 	public function logIn(){
 		 // バリデーション省略
 		if(Auth::attempt(Input::only('mail_address', 'password'))){
+			Session::put('mail_address', Input::get('mail_address'));
+			Session::put('password', Input::get('password'));
 			return Redirect::intended('/');
 		}
 		return Redirect::back()->withInput();		
