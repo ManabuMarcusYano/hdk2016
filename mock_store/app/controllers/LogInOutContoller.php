0eb7aa9804b641d21fe69347b7b074944548b362
Manabu_YANO <yano_manabu@applibot.co.jp>
2014-12-01 10:29:15 +0900

    セッションの記録位置変更

--- mock_store/app/controllers/LogInOutContoller.php
+++ mock_store/app/controllers/LogInOutContoller.php
@@ -3,9 +3,9 @@
 class LogInOutController extends BaseController{
 	public function logIn(){
 		 // バリデーション省略
+		 Session::put('mail_address', Input::get('mail_address'));
+		Session::put('password', Input::get('password'));
 		if(Auth::attempt(Input::only('mail_address', 'password'))){
-			Session::put('mail_address', Input::get('mail_address'));
-			Session::put('password', Input::get('password'));
 			return Redirect::intended('/');
 		}
 		return Redirect::to('/login');
